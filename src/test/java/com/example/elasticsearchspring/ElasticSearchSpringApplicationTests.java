a919d4e421d1682186552d5e06bcf0b7c13f0ffc
joaoRealinho <joao_ga_io@hotmail.com>
2023-05-17 22:40:57 +0100

    initial commint
    (cherry picked from commit 98d5e8c13c8d166bf5358c57699cda1a82a0e084)

---
+++ src/test/java/com/example/elasticsearchspring/ElasticSearchSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
